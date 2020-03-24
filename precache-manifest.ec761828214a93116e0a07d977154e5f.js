212922a1e6a6c9a041e34ad0555bc7803add6eb1
pawelkoper <koper84@gmail.com>
2020-03-24 10:57:34 +0100

    Updates

---
+++ precache-manifest.ec761828214a93116e0a07d977154e5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2aab0280a1060f50cc90ee97e157b0d",
+    "url": "/monsters-holodex/index.html"
+  },
+  {
+    "revision": "23fd23b2d71116e938ec",
+    "url": "/monsters-holodex/static/css/main.0f1716a1.chunk.css"
+  },
+  {
+    "revision": "a2c3c1b811bc825d6595",
+    "url": "/monsters-holodex/static/js/2.5ad06ca7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-holodex/static/js/2.5ad06ca7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23fd23b2d71116e938ec",
+    "url": "/monsters-holodex/static/js/main.4d8b5f67.chunk.js"
+  },
+  {
+    "revision": "03fd64d6426bcb1d56a7",
+    "url": "/monsters-holodex/static/js/runtime-main.5d6a577e.js"
+  }
+]);
