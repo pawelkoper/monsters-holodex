08fe92071b0d1fbb525dc8c111b8113d181bdc80
pawelkoper <koper84@gmail.com>
2020-03-24 10:48:31 +0100

    add css

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends React.Component {
             searchField.toLowerCase()));
     return (
         <div className="App">
+          <h1>Monsters Rolodex</h1>
           <SearchBox placeholder='search monsters'
                      handleChange={ this.handleChange}  />
           <CardList monsters={filteredMonsters}></CardList>
